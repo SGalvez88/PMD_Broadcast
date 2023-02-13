51bf1c6b8262f9cef74adb1ef60572cc49052d63
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2023-02-13 08:46:56 +0100

    2323

--- app/src/main/java/com/example/pmd_broadcast/MainActivity.java
+++ app/src/main/java/com/example/pmd_broadcast/MainActivity.java
@@ -9,7 +9,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     MiBroadcast miBroadcast = new MiBroadcast();
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
